1e56cf7fe3e370dd7db6ea98f4c842063e6c9f17
wbrunovieira <wbrunovieira77@gmail.com>
2022-10-12 12:11:45 +0100

    created the style.css and imported to the App.jsx, created the component Header usins css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './styles.css';
 
 function App() {
   
 
   return (
     <>
+      <Header />
+      
       <Post author="Bruno Vieira" content="post very nice"/>
       <Post author="Stephanie Vieira" content="Thats is so nice"/>
     
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <div>
+                Ignite Feed
+            </div>
+        </header>
+    )
+}

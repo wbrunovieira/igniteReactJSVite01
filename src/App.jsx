c218c3ba8022e6a37b1c580a7f186a0218495bce
wbrunovieira <wbrunovieira77@gmail.com>
2022-10-17 20:50:24 +0100

    started the component Sidebar and css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
+import {  Sidebar } from './components/Sidebar'
 
 import './global.css';
+import styles from './App.module.css';
 
 function App() {
   
@@ -9,9 +11,17 @@ function App() {
   return (
     <>
       <Header />
+        <div className={styles.wrapper}>
+          <Sidebar />
+        <main>
+          <Post author="Bruno Vieira" content="post very nice"/>
+          <Post author="Stephanie Vieira" content="Thats is so nice"/>
 
-      <Post author="Bruno Vieira" content="post very nice"/>
-      <Post author="Stephanie Vieira" content="Thats is so nice"/>
+        </main>
+      </div>
+    
+
+     
     
     </>
     )

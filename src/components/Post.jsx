2ea5888551eb68c0711859820121cdfda7c51a2b
wbrunovieira <wbrunovieira77@gmail.com>
2022-10-20 08:45:28 +0100

    it was missing the keys in the maps rendering

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -12,6 +12,7 @@ export function Post({ author, publishedAt, content }) {
       ]);
     
       const [newCommentText, setNewCommentText] = useState('');
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
       });
@@ -53,9 +54,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -79,7 +80,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
             {comments.map(comment => {
-          return <Comment content={comment} />
+          return <Comment key={comment} content={comment} />
         })}
       </div>
     </article>

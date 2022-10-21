b59474f05de893ec67384f633d4bc0787834fb61
wbrunovieira <wbrunovieira77@gmail.com>
2022-10-21 09:37:40 +0100

    made another directory, where created a new project in typescript, and migrated the js project to ts

---
+++ ignite-vite01Typescript/src/App.tsx
@@ -0,0 +1,72 @@
+import { Header } from './components/Header'
+import { Post, PostProps } from './components/Post'
+import {  Sidebar } from './components/Sidebar'
+
+import './global.css';
+import styles from './App.module.css';
+
+interface Post extends PostProps {
+  id: number;
+}
+
+const posts: Post[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/wbrunovieira.png',
+      name: 'Bruno Vieira',
+      role: 'CTO '
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-10-11 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-10-10 20:00:00'),
+  },
+];
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+        <div className={styles.wrapper}>
+          <Sidebar />
+        <main>
+          {posts.map(post => {
+              return (
+                <Post
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })}
+        </main>
+      </div>
+    
+
+     
+    
+    </>
+    )
+  }
+
+export default App
